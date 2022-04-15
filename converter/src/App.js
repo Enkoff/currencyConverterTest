105dc12cde4a9245632df06552fa0e4df086f28f
Enkoff <0639818413@ukr.net>
2022-04-15 12:42:35 +0300

    init project

---
+++ converter/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './component/header/Header';
+
+const App = () => {
+    return (
+        <div>
+        <Header />
+        </div>
+    );
+};
+
+export default App;
---
+++ converter/src/component/header/Header.js
@@ -0,0 +1,18 @@
+import React, {useEffect} from 'react';
+
+
+const Header = () => {
+
+
+    useEffect(() => {
+
+    });
+
+    return (
+        <div>
+            Hello
+        </div>
+    );
+}
+
+export default Header;
